ce5f1ce3170a6582080bbbcf1a7cbf73250c564e
MarcodenBoer <marco.denboer2000@gmail.com>
2020-10-12 11:37:20 +0200

    kan door foto's heen blijven scrollen

--- js/main.js
+++ js/main.js
@@ -2,12 +2,26 @@ const change_img = document.getElementById('js--change_img');
 const fadeFigure = document.getElementById('js--fadeFigure');
 const fadeFigure__image = document.getElementById('js--fadeFigure__image');
 
+const list = ["/img/Felipe.png", "/img/Moby-logo.png"];
+i = 0;
+
 
 change_img.onclick = () =>{
-  fadeFigure.style.filter = "blur(5px)";
+  fadeFigure.style.filter = "blur(5rem)";
   console.log("peepo");
-  setTimeout(function () {
-    fadeFigure__image.setAttribute("src", "/img/Moby-logo.png");
-    fadeFigure.style.filter = "blur(0)";
-  },1000)
+  if (i === 0) {
+    setTimeout(function () {
+      fadeFigure__image.setAttribute("src", list[i + 1]);
+      fadeFigure.style.filter = "blur(0)";
+      i = 1;
+    },3000);
+  }else if (i === 1) {
+    setTimeout(function () {
+      last = fadeFigure__image.getAttribute("src");
+      console.log(last);
+      fadeFigure__image.setAttribute("src", list[i - 1]);
+      fadeFigure.style.filter = "blur(0)";
+      i = 0;
+    },3000);
+  }
 }
