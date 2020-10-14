40fad6fc882af65e413e832d3c0ff9d192698bae
MarcodenBoer <marco.denboer2000@gmail.com>
2020-10-14 15:41:13 +0200

    pijltjes verbeterd, betere plaatjes, bubble navigatie toegevoegd, continue button erbij

--- js/main.js
+++ js/main.js
@@ -3,55 +3,68 @@ const change_img__backwards = document.getElementById('js--change_img--backwards
 const fadeFigure = document.getElementById('js--fadeFigure');
 const fadeFigure__image = document.getElementById('js--fadeFigure__image');
 const image__discription = document.getElementById('js--image__discription');
+const bubbles = document.getElementsByClassName('nav__bubbles__dot');
 
 image__discription.innerHTML = "";
+const dotColor__active = "#eee";
+const dotColor__notActive = "#3c3c3c";
 
-const list = ["/img/Felipe.png", "/img/Moby-logo.png"];
-const description_list = ["this be felipe :D", "this be a fish >:("]
+const list = ["/img/Forest.jpg", "/img/Moby-logo.png", "/img/Tower.jpg"];
+const description_list = ["Via dit bos pad zal ik bij het vluchtelingen kamp kunnen komen", "this be a fish >:(", "Vanaf deze toren heb ik een perfect uitzicht over het dorp"]
 let i = 0;
 let x = 0;
-
 
 change_img__forwards.onclick = () =>{
   fadeFigure.style.filter = "blur(5rem)";
   image__discription.innerHTML = "";
-  console.log("peepo");
+  change_img__forwards.disabled = true;
+  change_img__backwards.disabled = true;
   setTimeout(function () {
     if (i == list.length - 1) {
+      bubbles[i].style.backgroundColor = dotColor__active;
       i = 0;
       x = 0;
+      bubbles[i].style.backgroundColor = dotColor__notActive;
       fadeFigure__image.setAttribute("src", list[i]);
-      console.log(i);
-      console.log(x);
       typeText(Array.from(description_list[x]));
     }else {
+      bubbles[i].style.backgroundColor = dotColor__active;
+      bubbles[i + 1].style.backgroundColor = dotColor__notActive;
       i++;
       x++;
       fadeFigure__image.setAttribute("src", list[i]);
       typeText(Array.from(description_list[x]));
     }
       fadeFigure.style.filter = "blur(0)";
+      change_img__forwards.disabled = false;
+      change_img__backwards.disabled = false;
     },3000);
   }
 
 change_img__backwards.onclick = () =>{
   fadeFigure.style.filter = "blur(5rem)";
   image__discription.innerHTML = "";
+  change_img__forwards.disabled = true;
+  change_img__backwards.disabled = true;
   setTimeout(function () {
     if (i == 0) {
+      bubbles[i].style.backgroundColor = dotColor__active;
       i = list.length - 1;
       x = description_list.length - 1;
+      bubbles[i].style.backgroundColor = dotColor__notActive;
       fadeFigure__image.setAttribute("src", list[i]);
-      console.log(i);
-      console.log(x);
       typeText(Array.from(description_list[x]));
     }else {
+      bubbles[i].style.backgroundColor = dotColor__active;
+      bubbles[i - 1].style.backgroundColor = dotColor__notActive;
       i--;
       x--;
       fadeFigure__image.setAttribute("src", list[i]);
       typeText(Array.from(description_list[x]));
     }
       fadeFigure.style.filter = "blur(0)";
+      change_img__forwards.disabled = false;
+      change_img__backwards.disabled = false;
     },3000);
   }
 
