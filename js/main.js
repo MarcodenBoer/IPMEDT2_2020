c378177ec76229de559067091a3eb45ede12cb68
MarcodenBoer <marco.denboer2000@gmail.com>
2020-10-23 12:27:53 +0200

    button tekst aangepast, nieuwe pijltjes gemaakt

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const fadeFigure = document.getElementById('js--fadeFigure');
 const fadeFigure__image = document.getElementById('js--fadeFigure__image');
 const image__discription = document.getElementById('js--image__discription');
 const bubbles = document.getElementsByClassName('nav__bubbles__dot');
+const image__discription__button = document.getElementById("js--image__discription__button");
 
 image__discription.innerHTML = "";
 const dotColor__active = "#eee";
@@ -11,12 +12,14 @@ const dotColor__notActive = "#3c3c3c";
 
 const list = ["/img/Forest.jpg", "/img/Moby-logo.png", "/img/Tower.jpg"];
 const description_list = ["Via dit bos pad zal ik bij het vluchtelingen kamp kunnen komen", "this be a fish >:(", "Vanaf deze toren heb ik een perfect uitzicht over het dorp"]
+const locatie = ["het bospad", "de fisch", "de wachttoren"];
 let i = 0;
 let x = 0;
 
 change_img__forwards.onclick = () =>{
   fadeFigure.style.filter = "blur(5rem)";
   image__discription.innerHTML = "";
+  image__discription__button.innerHTML = "";
   change_img__forwards.disabled = true;
   change_img__backwards.disabled = true;
   setTimeout(function () {
@@ -27,6 +30,7 @@ change_img__forwards.onclick = () =>{
       bubbles[i].style.backgroundColor = dotColor__notActive;
       fadeFigure__image.setAttribute("src", list[i]);
       typeText(Array.from(description_list[x]));
+      image__discription__button.innerHTML = "Naar " + locatie[i];
     }else {
       bubbles[i].style.backgroundColor = dotColor__active;
       bubbles[i + 1].style.backgroundColor = dotColor__notActive;
@@ -34,6 +38,7 @@ change_img__forwards.onclick = () =>{
       x++;
       fadeFigure__image.setAttribute("src", list[i]);
       typeText(Array.from(description_list[x]));
+      image__discription__button.innerHTML = "Naar " + locatie[i];
     }
       fadeFigure.style.filter = "blur(0)";
       change_img__forwards.disabled = false;
@@ -54,6 +59,7 @@ change_img__backwards.onclick = () =>{
       bubbles[i].style.backgroundColor = dotColor__notActive;
       fadeFigure__image.setAttribute("src", list[i]);
       typeText(Array.from(description_list[x]));
+      image__discription__button.innerHTML = "Naar " + locatie[i];
     }else {
       bubbles[i].style.backgroundColor = dotColor__active;
       bubbles[i - 1].style.backgroundColor = dotColor__notActive;
@@ -61,6 +67,7 @@ change_img__backwards.onclick = () =>{
       x--;
       fadeFigure__image.setAttribute("src", list[i]);
       typeText(Array.from(description_list[x]));
+      image__discription__button.innerHTML = "Naar " + locatie[i];
     }
       fadeFigure.style.filter = "blur(0)";
       change_img__forwards.disabled = false;
